2474826ff3d3ff754889bfd1fcf9aaffe9eaf93e
ggines <95416761+ggines@users.noreply.github.com>
2022-02-02 17:38:09 +0100

    He fet canvis en el codi i ja funciona

--- Exercici 7/functions.c
+++ Exercici 7/functions.c
@@ -4,41 +4,76 @@
  *  Created on: 12 ene 2022
  *      Author: ubuntu
  */
-int sizeArray(int SIZE){
+#include <stdio.h>
+#include "functions.h"
+
+int validate(){
+	int size;
 	printf("Introdueix el tamany de l'array (entre 1 i 50): ");
-	scanf("%d", &SIZE);
-	while(SIZE>50 || SIZE<1){
-		printf("Torna a introduïr un tamany correcte (entre 1 i 50): ");
-		scanf("%d", &SIZE);
+	scanf("%d", &size);
+	while(size>50 || size<1){
+		printf("Torna a introduïr un tamany vàlid: ");
+		scanf("%d", &size);
 	}
-	return SIZE;
+	return size;
 }
 
-int media(int SIZE, int arr[SIZE], int i, float m){
-	int suma=0;
-	for(i=0;i<SIZE;i++){
+void fillArray(int *arr, int size){
+	int i;
+	for(i=0;i<size;i++){
+		printf("Introdueix els valors (entre 0 i 10): ");
+		scanf("%d", &arr[i]);
+		while(arr[i]>10 || arr[i]<0){
+			printf("Torna a introduïr un valor vàlid: ");
+			scanf("%d", &arr[i]);
+		}
+	}
+}
+
+void printArray(int *arr, int size){
+	int i, j, x;
+
+	for(i=0;i<size;i++){
+		for(j=i+1;j<size;j++){
+			if(arr[i]>arr[j]){
+				x= arr[i];
+				arr[i] = arr[j];
+				arr[j]= x;
+			}
+		}
+	}
+
+	for(i=0;i<size;i++){
+		printf("%d ", arr[i]);
+	}
+}
+
+void mitjanaArray(int *arr, int size){
+	int i, suma=0;
+	float m;
+	for(i=0;i<size;i++){
 		suma+=arr[i];
 	}
-	m= suma / SIZE;
-	return m;
+	m= suma / size;
+	printf("\nLa mitjana és %.2f", m);
 }
 
-int valorMax(int SIZE, int arr[SIZE], int i, int max){
-	max= arr[0];
-	for(i=1;i<SIZE;i++){
+void maxArray(int *arr, int size){
+	int i, max=arr[0];
+	for(i=1;i<size;i++){
 		if(arr[i]>max){
 			max= arr[i];
 		}
 	}
-	return max;
+	printf("\nEl valor màxim és %d", max);
 }
 
-int valorMin(int SIZE, int arr[SIZE], int i, int min){
-	min= arr[0];
-	for(i=1;i<SIZE;i++){
+void minArray(int *arr, int size){
+	int i, min=arr[0];
+	for(i=1;i<size;i++){
 		if(arr[i]<min){
 			min=arr[i];
 		}
 	}
-	return min;
+	printf("\nEl valor mínim és %d", min);
 }
--- Exercici 7/functions.h
+++ Exercici 7/functions.h
@@ -8,10 +8,7 @@
 #ifndef FUNCTIONS_H_
 #define FUNCTIONS_H_
 
-
-
 #endif /* FUNCTIONS_H_ */
-int sizeArray(int);
-int media(int, int, int, float);
-int valorMax(int, int, int, int);
-int valorMin(int, int, int, int);
+int validate();
+
+
